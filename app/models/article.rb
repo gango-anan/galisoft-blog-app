3ebb8fea4dabbee343f1b54e1eb83860f3896cc3
Galiwango Ananiya <galiwango101@gmail.com>
2022-02-04 23:16:04 +0300

    Add presence and length validations to the Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 3, maximum: 25}
+  validates :description, presence: true, length: {minimum: 10, maximum: 300}
 end
