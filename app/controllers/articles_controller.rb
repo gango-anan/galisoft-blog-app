62374a66b98182621fccac9d0211e86421dc806f
Galiwango Ananiya <galiwango101@gmail.com>
2022-02-08 02:26:37 +0300

    Refactor code to move redundant code into set_article method.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+
   def index
     @articles = Article.all
   end
@@ -7,9 +9,7 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def create
     @article = Article.new(article_params)
@@ -21,12 +21,9 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = 'Article updated successfully!'
       redirect_to article_path(@article)
@@ -36,7 +33,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted!"
     redirect_to articles_path
@@ -46,5 +42,9 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
