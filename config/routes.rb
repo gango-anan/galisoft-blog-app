97760d4c6cef82df72b8a54a221b02e6bbf9aff7
Galiwango Ananiya <galiwango101@gmail.com>
2022-02-09 23:09:14 +0300

    Add functionality to sign up new user

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
 end
