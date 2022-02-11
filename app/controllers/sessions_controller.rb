d429a417344ca42f4c02a7092d607d45e2fba1d2
Galiwango Ananiya <galiwango101@gmail.com>
2022-02-11 03:02:22 +0300

    Add user log in functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = 'You have successfully logged in!'
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = 'Wrong password or email, try again!'
+      render :new
+    end
+  end
+end
