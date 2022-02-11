e002581c39bc74c4bf0cc8041842125cd5794e98
Galiwango Ananiya <galiwango101@gmail.com>
2022-02-11 03:12:09 +0300

    Add user log out functionality.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,10 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'You have successfully logged out!'
+    redirect_to root_path
+  end
 end
