f86c456ed34efad3d819a039f2413c60b40ef114
Galiwango Ananiya <galiwango101@gmail.com>
2022-02-13 02:50:41 +0300

    Add funtionality to sign in user automatically after sign up and refactor user form partial to correctly display the required fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,8 +17,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Galisoft Blog #{@user.username.capitalize}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render :new
     end
@@ -36,7 +37,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 
   def set_user
