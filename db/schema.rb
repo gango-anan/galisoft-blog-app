9ee72c26cc3808e2f38ed42bc9caf4c74ea67f6a
Galiwango Ananiya <galiwango101@gmail.com>
2022-02-17 21:17:04 +0300

    Add admin field to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_175242) do
+ActiveRecord::Schema.define(version: 2022_02_17_175257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2022_02_09_175242) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
